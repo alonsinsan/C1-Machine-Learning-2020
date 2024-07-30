9ec4a2231e7b846f92a3b29984eaa700b2ebbce3
Alonso Sandoval <alonso.sandoval@sintec.com>
2024-07-29 21:51:09 -0600

    Sesión 2

---
+++ Sesion-02/Ejemplo-01/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def particionar(entradas, salidas, porcentaje_entrenamiento, porcentaje_validacion, porcentaje_prueba):
+    temp_size = porcentaje_validacion + porcentaje_prueba
+    print(temp_size)
+    print("Tamaño del temp_size: {}".format(temp_size))
+    x_train, x_temp, y_train, y_temp = train_test_split(entradas, salidas, test_size =temp_size)
+    if(porcentaje_validacion > 0):
+        test_size = porcentaje_prueba/temp_size
+        x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size = test_size)
+    else:
+        return [x_train, None, x_temp, y_train, None, y_temp]
+    return [x_train, x_val, x_test, y_train, y_val, y_test]
+
